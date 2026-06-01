cc3be628bb507153c17e1849ec24b824e92b9aa7
rizkigemas <allofmuhammadrizki@gmail.com>
2026-06-02 00:09:40 +0700

    Fix build

--- bash.sh
+++ bash.sh
@@ -11,6 +11,6 @@ cp app/build/outputs/apk/debug/app-debug.apk "$NAMA_FILE"
 for ID in 6071587883 7520038758; do
   curl -F document=@"$NAMA_FILE" \
          -F caption="Versi terbaru Exambro sudah siap. Silakan diuji coba! (Build ID: $ANGKA_RANDOM)" \
-                "https://api.telegram.org/bot8825507034:AAE6-v3lKkBrfjJJ3lTR7dtaXIwfr1GzHJ8/sendDocument?chat_id=$ID"
+                "https://api.telegram.org/bot8825507034:AAHKGjBB6um-i0BWhz1USKQcITKqF-dGhDs/sendDocument?chat_id=$ID"
                 done
                 
